76de3eab305423e226ec84e6d5e071ae312c316a
Devendra Saini <114621421+Devendrasaini1441@users.noreply.github.com>
2025-09-03 17:59:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,14 +26,15 @@ app.use("/api/payments", require("./routes/paymentRoutes"));
 
 // Serve React Frontend
 // ---------------------
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
